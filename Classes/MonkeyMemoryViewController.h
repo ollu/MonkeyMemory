3eba9df03081221c05cd3add0fa59e491cb62b3a
Ola Lundén <ola.lunden@gmail.com>
2011-03-23 11:38:54 +0100

    Hiding labels as well as tracking the right order

--- Classes/MonkeyMemoryViewController.h
+++ Classes/MonkeyMemoryViewController.h
@@ -10,12 +10,19 @@
 
 @interface MonkeyMemoryViewController : UIViewController {
     NSMutableArray *uniqueRandomNumbers;
+    NSMutableArray *correctOrder;
+    NSTimer *timer;
 }
 
 @property (nonatomic, retain) NSMutableArray *uniqueRandomNumbers;
+@property (nonatomic, retain) NSMutableArray *correctOrder;
+@property (nonatomic, retain) NSTimer *timer;
 
 - (void)setupButtons;
-- (NSMutableArray *)createRandomNumbers;
+- (void)hideLabels;
+- (void)init;
+- (NSMutableArray *)shuffle:(NSMutableArray *)mutableArray;
+- (NSMutableArray *)createArray:(int)withAmoutOfNumbers;
 
 - (IBAction)buttonPressed:(UIButton *)sender;
 
--- Classes/MonkeyMemoryViewController.m
+++ Classes/MonkeyMemoryViewController.m
@@ -11,45 +11,74 @@
 @implementation MonkeyMemoryViewController
 
 @synthesize uniqueRandomNumbers;
+@synthesize correctOrder;
+@synthesize timer;
 
 - (IBAction)buttonPressed:(UIButton *)sender {
+    if ([sender.titleLabel.text isEqual:[NSString stringWithFormat:@"%@", [correctOrder objectAtIndex:0]]]) {
+        [correctOrder removeObjectAtIndex:0];
+        sender.enabled = NO;
+    }
     NSLog(@"You pressed: %@", sender.titleLabel.text);
+    NSLog(@"The order is: %@", correctOrder);
 }
 
-- (NSMutableArray *)createRandomNumbers {
+- (NSMutableArray *)createArray:(int)withAmoutOfNumbers {
+    NSMutableArray *numbersInArray = [NSMutableArray arrayWithCapacity:withAmoutOfNumbers];
     
-    NSMutableArray *numbersInArray = [NSMutableArray arrayWithCapacity:9];
-
-    // Create array with numbers from 1 to 9
-    for ( int i = 1; i < 10; ++i ) {
+    // Create array with numbers from 1 to withAmoutOfNumbers
+    for ( int i = 1; i < withAmoutOfNumbers+1; ++i ) {
         [numbersInArray addObject:[NSNumber numberWithInt:i]];
     }
     
+    return numbersInArray;    
+}
+
+- (NSMutableArray *)shuffle:(NSMutableArray *)mutableArray {
     // Scramble the numbers in the array
     for ( int i = 0; i < 9; ++i ) {
         int randNumb = arc4random() % 9;
-        [numbersInArray exchangeObjectAtIndex:i withObjectAtIndex:randNumb];
+        [mutableArray exchangeObjectAtIndex:i withObjectAtIndex:randNumb];
     }
-    
-    return numbersInArray;
+    return mutableArray;
 }
 
 - (void)setupButtons {
 
-    uniqueRandomNumbers = [self createRandomNumbers];
+    uniqueRandomNumbers = [self shuffle:[self createArray:9]];
     
     for (int i = 1; i < 10; i++) {
         NSString *aTitle = [NSString stringWithFormat:@"%@", [uniqueRandomNumbers objectAtIndex:i-1]];
         
         UIButton *button = (UIButton *)[self.view viewWithTag:i];
         [button setTitle:aTitle forState:UIControlStateNormal];
+        [button setTitle:aTitle forState:UIControlStateDisabled];
     }
+}
+
+- (void)hideLabels {
+    for (int i = 1; i < 10; i++) {
+        
+        UIButton *button = (UIButton *)[self.view viewWithTag:i];
+        [button setTitle:@"" forState:UIControlStateNormal];
+    }
+}
+
+- (void)init {
+    [self setupButtons];
+    self.correctOrder = [self createArray:9];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:2 
+                                             target:self 
+                                           selector:@selector(hideLabels) 
+                                           userInfo:nil 
+                                            repeats:NO];    
 }
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupButtons];
+    [self init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -67,6 +96,9 @@
 
 - (void)dealloc {
     [super dealloc];
+    [uniqueRandomNumbers release];
+    [correctOrder release];
+    [timer release];
 }
 
 @end
